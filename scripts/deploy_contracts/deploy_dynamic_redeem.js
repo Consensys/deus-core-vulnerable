06436fc14a39f5763cfd61466e79688ad58608a4
smrm-dev <mrmousavi78@gmail.com>
2022-05-16 15:26:44 +0430

    Add DynamicRedeem deploy scripts

---
+++ scripts/deploy_contracts/deploy_dynamic_redeem.js
@@ -0,0 +1,37 @@
+const { deploy } = require('../helpers/deploy_contract.js')
+
+module.exports = async ({
+  deiAddress,
+  deusAddress,
+  collateralAddress,
+  muonAddress,
+  adminAddress,
+  minimumRequiredSignatures,
+  collateralRedemptionDelay,
+  deusRedemptionDelay,
+  poolCeiling,
+  libraryAddress,
+  appId,
+}) => {
+  const contractName = 'contracts/Pool/DynamicRedeem.sol:DynamicRedeem'
+  const deployer = process.env.MAIN_DEPLOYER
+  const deployedPool = await deploy({
+    deployer: deployer,
+    contractName: contractName,
+    constructorArguments: [
+      deiAddress,
+      deusAddress,
+      collateralAddress,
+      muonAddress,
+      libraryAddress,
+      adminAddress,
+      minimumRequiredSignatures,
+      collateralRedemptionDelay,
+      deusRedemptionDelay,
+      poolCeiling,
+      appId,
+    ],
+  })
+  const dynamicRedeemInstance = await hre.ethers.getContractFactory(contractName)
+  return dynamicRedeemInstance.attach(deployedPool.address)
+}
---
+++ scripts/deploy_dynamic_redeem.js
@@ -0,0 +1,49 @@
+const deployLibrary = require('./deploy_contracts/deploy_dei_pool_library.js')
+const deployDynamicRedeem = require('./deploy_contracts/deploy_pool.js')
+const { verifyAll } = require('./helpers/deploy_contract.js')
+const { config } = require('./network_config.js')
+const hre = require('hardhat')
+
+async function main() {
+  // * configuration
+  const deiAddress = '0xde12c7959e1a72bbe8a5f7a1dc8f8eef9ab011b3'
+  const deusAddress = '0xde5ed76e7c05ec5e4572cfc88d1acea165109e44'
+  const minimumRequiredSignatures = 1
+  const collateralRedemptionDelay = 30
+  const deusRedemptionDelay = 8 * 60 * 60
+  const appId = '20'
+
+  // const library = await deployLibrary({})
+  const libraryAddress = '0x219E8c4d2EA039d26647B80c424b1F91B427975F'
+
+  await new Promise((resolve) => setTimeout(resolve, 10000))
+  const conf = config[hre.network.name]
+  // * deployment
+  await deployDynamicRedeem({
+    deiAddress: deiAddress,
+    deusAddress: deusAddress,
+    collateralAddress: conf.collateralAddress,
+    muonAddress: conf.muonAddress,
+    adminAddress: conf.adminAddress,
+    minimumRequiredSignatures: minimumRequiredSignatures,
+    collateralRedemptionDelay: collateralRedemptionDelay,
+    deusRedemptionDelay: deusRedemptionDelay,
+    poolCeiling: conf.poolCeiling,
+    // libraryAddress: library.address,
+    libraryAddress: libraryAddress,
+    appId: appId,
+  })
+
+  await new Promise((resolve) => setTimeout(resolve, 30000))
+
+  await verifyAll()
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
